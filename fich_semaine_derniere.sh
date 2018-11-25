6a540afe0427e30dd21b7f6f7e0527de4c7e1fda
yasminebun <ybunout@gmail.com>
2018-11-25 16:00:41 +0100

    Ajout du script fich semaine dernière

---
+++ fich_semaine_derniere.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+jour=$(date +%d/%m/%Y)
+
+heure=$(date +%H:%M)
+
+echo "Hello  $USER, bienvenue à toi ! Aujourd'hui nous sommes le $jour et il est $heure."   
+
+read -p "Quel répertoire t'intéresse aujourd'hui ?" repertoire
+
+echo -n "Veux-tu connaitre le contenu de $repertoire ? Répondre par Y ou N :"
+read yesno
+if [ "$yesno" = "y" ] || [ "$yesno" = "Y" ]; then 
+	echo "Voici le contenu de $repertoire :"
+	cd ~
+	cd $repertoire
+	ls -al
+ 
+elif [ "$yesno" = "n" ] || [ "$yesno" = "N" ]; then
+	echo "D'accord, dans ce cas bonne journée à toi $USER !"
+
+else 
+	echo "Erreur de frappe ... Attention, il faut répondre par Y ou N !" 
+
+fi
